bc8bbf7543028594ad7d234832cd3eab4bb6e9b1
Andrew Abramov <stinger911@gmail.com>
2021-09-02 16:08:25 +0300

    Planet terrain generator

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,21 @@
-
-
 module.exports = api => {
   return {
     presets: [
       [
         '@quasar/babel-preset-app',
-        api.caller(caller => caller && caller.target === 'node')
-          ? { targets: { node: 'current' } }
-          : {}
-      ]
+        api.caller(caller => caller && caller.target === 'node') ?
+        {
+          targets: {
+            node: 'current'
+          }
+        } :
+        {}
+      ],
+      ['@babel/preset-env', {
+        targets: {
+          node: 'current'
+        }
+      }]
     ]
   }
 }
-
